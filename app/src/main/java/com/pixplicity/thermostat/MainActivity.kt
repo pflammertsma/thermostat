837fa41a6247ae557cfa289cd7eeca661e851dbf
Paul Lammertsma <paul@pixplicity.com>
2018-02-11 16:17:38 +0100

    set target temperature

--- app/src/main/java/com/pixplicity/thermostat/MainActivity.kt
+++ app/src/main/java/com/pixplicity/thermostat/MainActivity.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import java.io.IOException
 
@@ -11,16 +13,22 @@ import java.io.IOException
 private val TAG = MainActivity::class.java.simpleName
 
 class MainActivity : Activity() {
+    companion object {
+        private const val TEMP_MIN = 10f
+        private const val TEMP_MAX = 28f
+    }
 
     private val TAG = MainActivity::class.java!!.getSimpleName()
 
     private val handler = Handler()
-    private val reader = object: Runnable {
+    private val reader = object : Runnable {
         override fun run() {
             readData()
             handler.postDelayed(this, 500)
         }
     }
+
+    private var tempTarget = 20f
 
     private lateinit var tvTempCurrent: TextView
     private lateinit var tvTempTarget: TextView
@@ -31,12 +39,22 @@ class MainActivity : Activity() {
 
         tvTempCurrent = findViewById(R.id.tv_temp_current)
         tvTempTarget = findViewById(R.id.tv_temp_target)
+
+        findViewById<ImageButton>(R.id.bt_decrease).setOnClickListener {
+            tempTarget = Math.max(TEMP_MIN, Math.min(tempTarget - 0.5f, TEMP_MAX))
+            updateTarget()
+        }
+        findViewById<ImageButton>(R.id.bt_increase).setOnClickListener {
+            tempTarget = Math.max(TEMP_MIN, Math.min(tempTarget + 0.5f, TEMP_MAX))
+            updateTarget()
+        }
     }
 
     override fun onStart() {
         super.onStart()
 
         initSensor()
+        updateTarget()
         handler.post(reader)
     }
 
@@ -76,5 +94,8 @@ class MainActivity : Activity() {
         mBmp180 = null
     }
 
-}
+    private fun updateTarget() {
+        tvTempTarget.text = String.format("%.1f", tempTarget)
+    }
 
+}
