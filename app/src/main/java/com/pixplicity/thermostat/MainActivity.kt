5dfab978f34a1b26dbfdea5a7e7969dbee6e2a84
Paul Lammertsma <paul@pixplicity.com>
2018-02-11 16:06:17 +0100

    BTMP180 working

--- app/src/main/java/com/pixplicity/thermostat/MainActivity.kt
+++ app/src/main/java/com/pixplicity/thermostat/MainActivity.kt
@@ -1,76 +1,79 @@
 package com.pixplicity.thermostat
 
 import android.app.Activity
-import android.content.Context
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
-import android.hardware.SensorManager.DynamicSensorCallback
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
-import com.google.android.things.contrib.driver.bmx280.Bmx280SensorDriver
+import android.widget.TextView
 import java.io.IOException
 
 
 private val TAG = MainActivity::class.java.simpleName
 
-class MainActivity : Activity(), SensorEventListener {
+class MainActivity : Activity() {
 
     private val TAG = MainActivity::class.java!!.getSimpleName()
 
-    private var mTemperatureSensorDriver: Bmx280SensorDriver? = null
-    private lateinit var mSensorManager: SensorManager
-
-    private val mDynamicSensorCallback = object : DynamicSensorCallback() {
-        override fun onDynamicSensorConnected(sensor: Sensor) {
-            if (sensor.getType() === Sensor.TYPE_AMBIENT_TEMPERATURE) {
-                Log.i(TAG, "Temperature sensor connected")
-                mSensorManager!!.registerListener(this@MainActivity,
-                        sensor, SensorManager.SENSOR_DELAY_NORMAL)
-            }
+    private val handler = Handler()
+    private val reader = object: Runnable {
+        override fun run() {
+            readData()
+            handler.postDelayed(this, 500)
         }
     }
+
+    private lateinit var tvTempCurrent: TextView
+    private lateinit var tvTempTarget: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i(TAG, "Starting TemperatureActivity")
+        setContentView(R.layout.activity_main)
 
-        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        mSensorManager.registerDynamicSensorCallback(mDynamicSensorCallback)
+        tvTempCurrent = findViewById(R.id.tv_temp_current)
+        tvTempTarget = findViewById(R.id.tv_temp_target)
+    }
 
+    override fun onStart() {
+        super.onStart()
+
+        initSensor()
+        handler.post(reader)
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        handler.removeCallbacks(reader)
+        closeSensor()
+    }
+
+    private var mBmp180: Bmp180? = null
+    private val mI2cPort = BoardDefaults.getI2CPort()
+
+    private fun initSensor() {
+        mBmp180 = Bmp180(mI2cPort)
+    }
+
+    private fun readData() {
         try {
-            mTemperatureSensorDriver = Bmx280SensorDriver(BoardDefaults.getI2CPort())
-            mTemperatureSensorDriver!!.registerTemperatureSensor()
+            val temp = mBmp180!!.readTemperature()
+            val press = mBmp180!!.readPressure().toFloat()
+            val alt = mBmp180!!.readAltitude().toDouble()
+            Log.d(TAG, "loop: temp $temp alt: $alt press: $press")
+            tvTempCurrent.text = String.format("%.1f", temp)
         } catch (e: IOException) {
-            Log.e(TAG, "Error configuring sensor", e)
-        }
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(TAG, "Closing sensor")
-        if (mTemperatureSensorDriver != null) {
-            mSensorManager.unregisterDynamicSensorCallback(mDynamicSensorCallback)
-            mSensorManager.unregisterListener(this)
-            mTemperatureSensorDriver!!.unregisterTemperatureSensor()
-            try {
-                mTemperatureSensorDriver!!.close()
-            } catch (e: IOException) {
-                Log.e(TAG, "Error closing sensor", e)
-            } finally {
-                mTemperatureSensorDriver = null
-            }
+            Log.e(TAG, "Sensor loop  error : ", e)
         }
     }
 
-    override fun onSensorChanged(event: SensorEvent) {
-        Log.i(TAG, "sensor changed: " + event.values[0])
-    }
+    private fun closeSensor() {
+        try {
+            mBmp180!!.close()
+        } catch (e: IOException) {
+            Log.e(TAG, "closeSensor  error: ", e)
+        }
 
-    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
-        Log.i(TAG, "sensor accuracy changed: " + accuracy)
+        mBmp180 = null
     }
 
 }
